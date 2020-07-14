bc2eaedf9b51851199174d255ca762c3411a611d
BenWix <44454993+BenWix@users.noreply.github.com>
2020-07-14 18:31:56 -0400

    adds data from weather api to html

--- any_activity_frontend/index.js
+++ any_activity_frontend/index.js
@@ -1,4 +1,5 @@
 const BASE_URL = "http://localhost:3000"
+let degrees = "F"
 
 document.addEventListener("DOMContentLoaded", () => {
     let long; 
@@ -11,9 +12,30 @@ document.addEventListener("DOMContentLoaded", () => {
             let weatherURL = BASE_URL + `/weather/${lat}/${long}`
             fetch(weatherURL)
                 .then(response => response.json())
-                .then(json => console.log(json))
+                .then(json => setWeather(json))
         })
     } else {
         console.log("Unable to determine location")
     }
 })
+
+function setWeather(json) {
+    let weatherHeader = document.querySelector('.weather')
+    let temperature = document.createElement("li")
+    temperature.classList.add("temperature")
+    temperature.innerHTML = get_temp(json) + degrees
+    weatherHeader.appendChild(temperature)
+}
+
+function get_temp(weather) {
+    let rawTemp = weather["main"]["temp"]
+    let temp
+    //Temperature from API is in KELVIN
+    //Converts based on preference of fahrenheit or celsius
+    if (degrees === "F") {
+        temp = (rawTemp - 273.15) * 9/5 + 32
+    } else {
+        temp = rawTemp - 273.15
+    }
+    return Math.round(temp)
+}
