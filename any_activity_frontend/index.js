58d7df9279a378d7e0548f12a25ce7454112946a
BenWix <44454993+BenWix@users.noreply.github.com>
2020-07-15 09:39:21 -0400

    refactoring Weather functions

--- any_activity_frontend/index.js
+++ any_activity_frontend/index.js
@@ -1,37 +1,32 @@
 const BASE_URL = "http://localhost:3000"
-let degrees = "F"
+// let degrees = "F"
 let currentWeather
 
 class Weather {
-    constructor(temp, location, condition, icon) {
-        this.temp = temp
-        this.location = location
-        this.condition = condition
-        this.icon = icon
+    constructor(json) {
+        this._temp = json['main']['temp']
+        this.location = json['name']
+        this.condition = json['weather']['description']
+        this.icon = json['weather']['icon']
+        this.degrees = "F"
     }
 
-    static getWeather(lat, long) {
-        let weatherURL = BASE_URL + `/weather/${lat}/${long}`
-        fetch(weatherURL)
-            .then(response => response.json())
-            .then(json => Weather.setWeather(json))
-    }
-
-    static setWeather(json) {
     
+    setWeather() {
         let weatherHeader = document.querySelector('.weather')
-        let temperature = document.createElement("li")
+        let temperature = document.createElement("p")
         temperature.classList.add("temperature")
-        temperature.innerHTML = Weather.get_temp(json) + degrees
+        temperature.innerHTML = this.get_temp() + this.degrees
         weatherHeader.appendChild(temperature)
+        console.log('Weather has been set')
     }
     
-    static get_temp(weather) {
-        let rawTemp = weather["main"]["temp"]
+    get_temp() {
+        let rawTemp = this._temp
         let temp
         //Temperature from API is in KELVIN
         //Converts based on preference of fahrenheit or celsius
-        if (degrees === "F") {
+        if (this.degrees === "F") {
             temp = (rawTemp - 273.15) * 9/5 + 32
         } else {
             temp = rawTemp - 273.15
@@ -40,6 +35,16 @@ class Weather {
     }
 }
 
+function getWeather(lat, long) {
+    let weatherURL = BASE_URL + `/weather/${lat}/${long}`
+    fetch(weatherURL)
+        .then(response => response.json())
+        .then(json => {
+            console.log('Weather has been retrieved')
+            currentWeather = new Weather(json)
+            currentWeather.setWeather()
+        })
+}
 document.addEventListener("DOMContentLoaded", () => {
     let long; 
     let lat; 
@@ -47,13 +52,16 @@ document.addEventListener("DOMContentLoaded", () => {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude.toString(10).replace('.','x')
             lat = position.coords.latitude.toString(10).replace('.','x')
-            Weather.getWeather(lat,long)
+            console.log(`${lat}, ${long}`)
+            getWeather(lat,long)
         })
     } else {
         console.log("Unable to determine location, using default of New York City")
         long = 40.7128
         lat = -74.0060
-        Weather.getWeather(lat,long)
+        console.log(`${lat}, ${long}`)
+
+        getWeather(lat,long)
     }
 })
 
