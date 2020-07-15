6b244b51ca9d4442b7d91cb9db44f74603982d76
BenWix <44454993+BenWix@users.noreply.github.com>
2020-07-15 09:10:48 -0400

    Moves static functions into weather class

--- any_activity_frontend/index.js
+++ any_activity_frontend/index.js
@@ -1,5 +1,6 @@
 const BASE_URL = "http://localhost:3000"
 let degrees = "F"
+let currentWeather
 
 class Weather {
     constructor(temp, location, condition, icon) {
@@ -7,6 +8,35 @@ class Weather {
         this.location = location
         this.condition = condition
         this.icon = icon
+    }
+
+    static getWeather(lat, long) {
+        let weatherURL = BASE_URL + `/weather/${lat}/${long}`
+        fetch(weatherURL)
+            .then(response => response.json())
+            .then(json => Weather.setWeather(json))
+    }
+
+    static setWeather(json) {
+    
+        let weatherHeader = document.querySelector('.weather')
+        let temperature = document.createElement("li")
+        temperature.classList.add("temperature")
+        temperature.innerHTML = Weather.get_temp(json) + degrees
+        weatherHeader.appendChild(temperature)
+    }
+    
+    static get_temp(weather) {
+        let rawTemp = weather["main"]["temp"]
+        let temp
+        //Temperature from API is in KELVIN
+        //Converts based on preference of fahrenheit or celsius
+        if (degrees === "F") {
+            temp = (rawTemp - 273.15) * 9/5 + 32
+        } else {
+            temp = rawTemp - 273.15
+        }
+        return Math.round(temp)
     }
 }
 
@@ -17,35 +47,15 @@ document.addEventListener("DOMContentLoaded", () => {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude.toString(10).replace('.','x')
             lat = position.coords.latitude.toString(10).replace('.','x')
-
-            let weatherURL = BASE_URL + `/weather/${lat}/${long}`
-            fetch(weatherURL)
-                .then(response => response.json())
-                .then(json => setWeather(json))
+            Weather.getWeather(lat,long)
         })
     } else {
-        console.log("Unable to determine location")
+        console.log("Unable to determine location, using default of New York City")
+        long = 40.7128
+        lat = -74.0060
+        Weather.getWeather(lat,long)
     }
 })
 
-function setWeather(json) {
-    
-    let weatherHeader = document.querySelector('.weather')
-    let temperature = document.createElement("li")
-    temperature.classList.add("temperature")
-    temperature.innerHTML = get_temp(json) + degrees
-    weatherHeader.appendChild(temperature)
-}
 
-function get_temp(weather) {
-    let rawTemp = weather["main"]["temp"]
-    let temp
-    //Temperature from API is in KELVIN
-    //Converts based on preference of fahrenheit or celsius
-    if (degrees === "F") {
-        temp = (rawTemp - 273.15) * 9/5 + 32
-    } else {
-        temp = rawTemp - 273.15
-    }
-    return Math.round(temp)
-}
+
