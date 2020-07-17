0593872f7e611c440ea3a77797b7a50f6001e0f7
BenWix <44454993+BenWix@users.noreply.github.com>
2020-07-17 17:58:48 -0400

    Add more examples to seed data

--- any_activity_backend/db/seeds.rb
+++ any_activity_backend/db/seeds.rb
@@ -13,9 +13,32 @@ read.conditions.create(weather: "rainy", min_temp: -100, max_temp: 100)
 
 run = Activity.create(name: "go for a run")
 run.conditions.create(weather: "clear", min_temp: 50, max_temp: 90)
+run.conditions.create(weather: "cloudy", min_temp: 50, max_temp: 90)
+
 
 nap = Activity.create(name: "take a nap")
 nap.conditions.create(weather: "cloudy", min_temp: -100, max_temp: 100)
+nap.conditions.create(weather: "rainy", min_temp: -100, max_temp: 100)
+nap.conditions.create(weather: "misting", min_temp: -100, max_temp: 100)
+
+museum = Activity.create(name: "go to a museum")
+museum.conditions.create(weather: "cloudy", min_temp: -100, max_temp: 100)
+museum.conditions.create(weather: "rainy", min_temp: -100, max_temp: 100)
+museum.conditions.create(weather: "misting", min_temp: -100, max_temp: 100)
+
 
 sledding = Activity.create(name: "go sledding")
-sledding.conditions.create(weather: "snowing", min_temp: 0, max_temp: 30)
+sledding.conditions.create(weather: "snowing", min_temp: -100, max_temp: 30)
+
+bike = Activity.create(name: "go for a bike ride")
+bike.conditions.create(weather: "clear", min_temp: 45, max_temp: 100)
+bike.conditions.create(weather: "cloudy", min_temp: 45, max_temp: 100)
+
+watch = Activity.create(name: "watch the storm roll in")
+watch.conditions.create(weather: "storming", min_temp: -100, max_temp: 100)
+
+swimming = Activity.create(name: "go for a swim")
+swimming.conditions.create(weather: "clear", min_temp: 50, max_temp: 100)
+
+yoga = Activity.create(name: "do some yogay")
+yoga.conditions.create(weather: "any", max_temp: 100, min_temp:-100)
